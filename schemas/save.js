d848a11c21a83ea709bc09189c23181b4e209cb2
xexino <89513742+xexino@users.noreply.github.com>
2022-01-05 23:36:50 +0100

    second  commit

---
+++ schemas/save.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'save',
+    title: 'Save',
+    type: 'document',
+    fields: [
+        {
+            name: 'postedby',
+            title: 'PostedBy',
+            type: 'postedBy'
+        },
+        {
+            name: 'userId',
+            title: 'UserId',
+            type: 'string'
+        },
+    ]
+}
